a0ebdea3707ad229364101940449ef1ef2a43b9c
zrcoy <zhangruichaotyler@gmail.com>
2021-07-02 15:38:10 -0400

    initial implementation of a store service

--- server/save-course.route.ts
+++ server/save-course.route.ts
@@ -3,27 +3,25 @@ import { COURSES } from "./db-data";
 import { setTimeout } from "timers";
 
 export function saveCourse(req: Request, res: Response) {
-  console.log("ERROR saving course!");
-  res.sendStatus(500);
-  return;
+  // console.log("ERROR saving course!");
+  // res.sendStatus(500);
+  // return;
 
-  // const id = req.params["id"],
-  //     changes = req.body;
+  const id = req.params["id"],
+    changes = req.body;
 
-  // console.log("Saving course changes", id, JSON.stringify(changes));
+  console.log("Saving course changes", id, JSON.stringify(changes));
 
-  // const newCourse = {
-  //   ...COURSES[id],
-  //   ...changes
-  // };
+  const newCourse = {
+    ...COURSES[id],
+    ...changes,
+  };
 
-  // COURSES[id] = newCourse;
+  COURSES[id] = newCourse;
 
-  // console.log("new course version", newCourse);
+  console.log("new course version", newCourse);
 
-  // setTimeout(() => {
-
-  //     res.status(200).json(COURSES[id]);
-
-  // }, 2000);
+  setTimeout(() => {
+    res.status(200).json(COURSES[id]);
+  }, 2000);
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,7 @@ import { Course, sortCoursesBySeqNo } from "../model/course";
 import { CoursesService } from "../services/courses.service";
 import { LoadingService } from "../loading/loading.service";
 import { MessagesService } from "../messages/messages.service";
+import { CoursesStore } from "../services/courses.store";
 
 @Component({
   selector: "home",
@@ -27,44 +28,18 @@ export class HomeComponent implements OnInit {
 
   advancedCourses$: Observable<Course[]>;
 
-  constructor(
-    private coursesService: CoursesService,
-    private loadingService: LoadingService,
-    private messagesService: MessagesService
-  ) {}
+  constructor(private coursesStore: CoursesStore) {}
 
   ngOnInit() {
     this.reloadCourses();
   }
 
   reloadCourses() {
-    const courses$ = this.coursesService.getAllCourses().pipe(
-      map((courses) => courses.sort(sortCoursesBySeqNo)),
-      catchError((err) => {
-        const message = "Could not load courses";
-        this.messagesService.showErrors(message);
-        console.log(message, err);
-        //have to return an obs to replace the original one and terminate!
-        return throwError(err);
-      })
-    );
-
-    const loadedCourses$ =
-      this.loadingService.showLoadingUntilCompleted(courses$);
-
     //testing purposes for accidentally sending extra http request
     // courses$.subscribe((data) => console.log(data));
 
-    this.beginnerCourses$ = loadedCourses$.pipe(
-      map((courses) =>
-        courses.filter((course) => course.category === "BEGINNER")
-      )
-    );
+    this.beginnerCourses$ = this.coursesStore.filterByCategory("BEGINNER");
 
-    this.advancedCourses$ = loadedCourses$.pipe(
-      map((courses) =>
-        courses.filter((course) => course.category === "ADVANCED")
-      )
-    );
+    this.advancedCourses$ = this.coursesStore.filterByCategory("ADVANCED");
   }
 }
